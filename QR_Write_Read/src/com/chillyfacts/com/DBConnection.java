8a020e9c74aa86cf03eddf04ed6c78412677db8f
Ashutosh Kumar <66332175+Ashugee2825@users.noreply.github.com>
2023-08-14 10:03:47 +0530

    Add files via upload

---
+++ QR_Write_Read/src/com/chillyfacts/com/DBConnection.java
@@ -0,0 +1,22 @@
+
+
+package com.chillyfacts.com;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DBConnection {        
+	public Connection getConnection(){          
+		Connection connection=null;                      
+		System.out.println("Connection called");                  
+		try {                                                                  
+			Class.forName("com.mysql.jdbc.Driver");              
+		connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sam","root", "nitr@123");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+}
+ 
